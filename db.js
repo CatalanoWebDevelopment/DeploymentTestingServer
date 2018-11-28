d83e5582fb41c808605edb6a3db000a7287e5c9d
Parker K Catalano <parkerkcatalano@Admins-MacBook-Pro.local>
2018-11-28 15:06:34 -0500

    Modified DB to include DB_URL from .env file

--- db.js
+++ db.js
@@ -1,22 +1,16 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize(
-  "test-deployment",
-  "postgres",
-  process.env.PG_SECRET,
-  {
-    host: "localhost",
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: "postgres"
-  }
-);
+})
 
 sequelize.authenticate().then(
-    function() {
-        console.log("Connected to TestDeployment Postgres Database.")
-    },
-    function(err) {
-        console.log(err)
-    }
+  function() {
+    console.log("Connected to TestDeployment Postgres Database.");
+  },
+  function(err) {
+    console.log(err);
+  }
 );
 
 module.exports = sequelize;
