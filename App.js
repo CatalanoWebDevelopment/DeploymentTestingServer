a90e8fc2707f01e7d406545ee6d4596a58d4a414
Parker K Catalano <parkerkcatalano@Admins-MacBook-Pro.local>
2018-11-28 15:19:51 -0500

    Cleaned Up App.js

--- App.js
+++ App.js
@@ -5,12 +5,10 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 
-const port = 3000;
-
 const HelloWorld = require("./controllers/HelloWorldController");
 
 app.use(bodyParser.json());
 app.use(require("./middleware/headers"));
 app.use("/hello_world", HelloWorld);
 
-app.listen(port);
+app.listen(process.env.PORT);
