9701f1452eaf41a349f65fb31752ae4a6196e847
Parker K Catalano <parkerkcatalano@Admins-MacBook-Pro.local>
2018-11-29 13:53:01 -0500

    UserController: Added User Login and User Create

---
+++ controllers/UserController.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const router = express.Router();
+const sequelize = require("../db");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const validateSession = require("../middleware/validate-session");
+const User = sequelize.import("../models/user");
+
+/* CREATE USER */
+router.post("/create", function(req, res) {
+  let email = req.body.user.email;
+  let firstName = req.body.user.firstName;
+  let lastName = req.body.user.lastName;
+  let password = req.body.user.password;
+
+  User.create({
+    email,
+    firstName,
+    lastName,
+    password: bcrypt.hashSync(password, 10)
+  }).then(
+    function createSuccess(user) {
+      let token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+        expiresIn: "24h"
+      });
+
+      res.json({
+        user,
+        message: "User Created",
+        sessionToken: token
+      });
+    },
+    function createError(err) {
+      res.send(500, err.message);
+    }
+  );
+});
+
+/* LOGIN USER */
+router.post("/login", function(req, res) {
+  User.findOne({ where: { email: req.body.user.email } }).then(
+    function(user) {
+      if (user) {
+        bcrypt.compare(req.body.user.password, user.password, function(
+          err,
+          matches
+        ) {
+          if (matches) {
+            let token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+              expiresIn: "24h"
+            });
+
+            res.json({
+              user,
+              message: "Successfully Authenticated.",
+              sessionToken: token
+            });
+          } else {
+            res.status(502).send({ error: "Username or Password is Invalid." });
+          }
+        });
+      } else {
+        res.status(500).send({ error: "You have failed to authenticate." });
+      }
+    },
+    function(err) {
+      res.status(501).send({ error: "Login failed. Please try again." });
+    }
+  );
+});
---
+++ models/user.js
@@ -0,0 +1,8 @@
+module.exports = function(sequelize, DataTypes) {
+    return sequelize.define('user', {
+        email: DataTypes.STRING,
+        firstName: DataTypes.STRING,
+        lastName: DataTypes.STRING,
+        password: DataTypes.STRING
+    })
+}
